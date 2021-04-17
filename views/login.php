332ae83de235075ce6e33852b0ecbe6032fb7474
FelipeAdm <FelipeAdm@ALPINO.LOCAL>
2021-04-17 11:19:24 -0300

    adicionando imagem fundo login

--- views/login.php
+++ views/login.php
@@ -1,20 +1,21 @@
-<?php 
+<?php
 require '../php_controller/UserDaoMysql.php';
 session_start();
 
 $UserDao = new UserDaoMysql($pdo);
 
-if(isset($_SESSION['email'])){
+if (isset($_SESSION['email'])) {
     $isLogged = $UserDao->isLogged($_SESSION['email']);
-    if($isLogged){
+    if ($isLogged) {
         header('Location:/dashboard/MGpiscinas/myFirstDashboard/views/dashboard.php');
         exit;
-    } 
+    }
 }
 
 ?>
 
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,21 +24,22 @@ if(isset($_SESSION['email'])){
     <link rel="stylesheet" href="../plugins/fontawesome5.15.1/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="../images/dash.ico" />
     <title>My Dashboard</title>
 </head>
 
-<body>
+<body class="body-login-bgimage">
     <div class="flex-page">
         <div class="side-bar">
-            <form  method="POST" action="../php_controller/login_action.php" class="form-index-login" id="form-index-login">
+            <form method="POST" action="../php_controller/login_action.php" class="form-index-login" id="form-index-login">
                 <div class="form-group">
-                    <?php 
-                        if(isset($_SESSION['flash']) && $_SESSION['flash'] != ''){
-                            echo '<div class="flash-warning">';
-                            print_r($_SESSION['flash']);
-                            $_SESSION['flash'] = '';
-                            echo '</div>'; 
-                        }
+                    <?php
+                    if (isset($_SESSION['flash']) && $_SESSION['flash'] != '') {
+                        echo '<div class="flash-warning">';
+                        print_r($_SESSION['flash']);
+                        $_SESSION['flash'] = '';
+                        echo '</div>';
+                    }
                     ?>
 
                     <label for="email-input-index">Email address</label>
@@ -47,10 +49,9 @@ if(isset($_SESSION['email'])){
                     <label for="exampleInputPassword1">Senha</label>
                     <input type="password" name="formIndexSenha" class="form-control" id="senha-form-index" placeholder="Digite sua senha">
                 </div>
-                <button type="submit"  class="btn btn-submit-forms">Entrar</button>
+                <button type="submit" class="btn btn-submit-forms">Entrar</button>
             </form>
         </div>
-        <!-- <div class="main-content"> </div>  -->
     </div>
     <script src="../plugins/jquery-3.5.1/jquery-3.5.1.js"></script>
     <script src="../plugins/bootstrap-4.5.3/js/bootstrap.min.js"></script>
@@ -60,4 +61,5 @@ if(isset($_SESSION['email'])){
     <script type="module" src="../js/script.js"></script>
 
 </body>
+
 </html>
